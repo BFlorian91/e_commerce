ac7f97538fa80f51112ff6d915d77b1b1af2d604
Florian <flbeaumo@student.42.fr>
2019-10-02 11:00:07 +0200

    init

---
+++ controller/index.php
@@ -0,0 +1,12 @@
+<?php
+	require 'model/cart.php';
+	require 'model/notFound.php';
+
+	//function cart() {
+		//require('cart.php');
+	//}
+
+	function notFound() {
+		require('404.php');
+	}
+?>
---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+	require('controller/index.php');
+
+	echo '<h1>HOME SWEET HOME</h1>';
+
+	if (isset($_GET['action'])) {
+		if ($_GET['action'] == 'cart') {
+			cart();	
+			echo 'TEST';
+		} else {
+			notFound();
+		}
+	}
+?>
