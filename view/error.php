34a4e848976775c322fca0943143974407c7f2a2
Florian <flbeaumo@student.42.fr>
2019-10-10 10:44:15 +0200

    Login ✅ ; Register ✅ ; logout 🚼

--- view/error.php
+++ view/error.php
@@ -1,7 +1,7 @@
 <?php
 	function error_($content) {
 		$html_element .= '<div id="error">';
-		$html_element .= '<h3 style="color: red;">' . $content . '</h3>';
+		$html_element .= '<h3 style="color: red; font-weight: bold; font-family: Arial;">' . $content . '</h3>';
 		$html_element .= '</div>';
 
 		return $html_element;
--- view/header.php
+++ view/header.php
@@ -1,5 +1,5 @@
 <?php
-	function header_page() {
+	function header_page($username = null) {
 		$html_element .= "<html>";
 		$html_element .= "<head>";
 		$html_element .= "<meta charset=\"UTF-8\" />";
@@ -11,8 +11,13 @@
 		if ($_SESSION['username'] == 'admin') {
 			$html_element .= "<a href=\"index.php?action=panel\" id=\"manage_shop\"><button class=\"button-loggin\">Manage shop</button></a>";
 		}
+		if (empty($_SESSION['username'])) {
 		$html_element .= "<a href=\"index.php?action=signup\" id=\"signup\"><button class=\"button-loggin\">Sign up</button></a>";
 		$html_element .= "<a href=\"index.php?action=login\" id=\"login\"><button class=\"button-loggin\">login</button></a>";
+		} else {
+			$html_element .= "Hello " . $username . "  ";
+			$html_element .= "<a href=\"index.php?action=logout\" id=\"logout\"><button class=\"button-loggin\">Logout</button></a>";
+		}
 		$html_element .= "<div id=\"website_name\">The Rainbow Market</div>";
 		$html_element .= "</div>";
 		return $html_element;
